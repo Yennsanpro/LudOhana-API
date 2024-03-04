e230b78b3576d5443bf56170a08232928912ee1c
Yennyth Sánchez <yennyth.es@gmail.com>
2024-03-04 14:52:49 +0000

    models validate

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,46 +1,63 @@
-const{ DataTypes } = require('sequelize')
+const { DataTypes } = require('sequelize')
 const sequelize = require('../../db')
 
-const UserModel = sequelize.define('user', { 
+const IMAGE_EXTENSIONS = ['png', 'jpeg', 'jpg', 'webdp']
+
+function hasValidImageExtension(path) { //function created for validate image
+    const lowerCasePath = path.toLowerCase();
+    return IMAGE_EXTENSIONS(path).some((extension) => lowerCasePath.endsWith(`${ extension }`))
+}
+
+const UserModel = sequelize.define('user', {
     name: {
         type: DataTypes.STRING,
         allowNull: false,
-    
+        validate: {
+            len: [1, 155] //minimum and maximum validation
+        }
     },
     lastName: {
         type: DataTypes.STRING,
         allowNull: false,
-    
+        validate: {
+            len: [1, 155] //minimum and maximum validation
+        }
     },
-    email:{
+    email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
     },
     password: {
         type: DataTypes.STRING,
-        allowNull:false
+        allowNull: false
     },
     role: {
-        type:DataTypes.STRING,
+        type: DataTypes.STRING,
         defaultValue: 'user',
         validate: {
             isIn: [[undefined, 'user', 'admin']] //define the admin roll
-            }
-        },
+        }
+    },
     location: {
-         type: DataTypes.STRING,
-        allowNull:false
+        type: DataTypes.STRING,
+        allowNull: false
     },
     description: {
         type: DataTypes.STRING,
-       allowNull:false
-   },
-   image: {
-    type: DataTypes.STRING,
-   allowNull:false
-},
-    })
 
-    
+    },
+    image: {
+        type: DataTypes.STRING,
+        validate: { //on the top has the function to make this validate
+            endsWithValidImageExtension(value) {
+                if (!hasValidImageExtension(value)) {
+                    throw new Error('Img should have these extensions: ' + `${[...IMAGE_EXTENSIONS]}`);
+                }
+            }
+        }
+    },
+})
+
+
 module.exports = UserModel

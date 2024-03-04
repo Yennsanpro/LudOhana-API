9d38dba3f5bc342f99ec61b90c3a7700f57af448
Yennyth Sánchez <yennyth.es@gmail.com>
2024-03-04 14:01:02 +0000

    updateUser created/tested

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -8,23 +8,23 @@ const signup = async (req, res) => { //function user can signup
         const salt = bcrypt.genSaltSync(parseInt(process.env.BCRYPT_SALT))
         req.body.password = bcrypt.hashSync(req.body.password, salt)
 
-        const user = await UserModel.create(req.body) 
+        const user = await UserModel.create(req.body)
 
         const token = jwt.sign( // token created
-            {email: user.email},
-            
+            { email: user.email },
+
             process.env.JWT_SECRET,//secret word
             //{ expiresIn: '7d' } //solo se pone al terminar la aplicación
         )
-      
-        res.status(200).json({token: token, message: 'Account created'})
+
+        res.status(200).json({ token: token, message: 'Account created' })
     } catch (error) {
         console.log(error)
         res.status(500).send('Error Signing up')
     }
-} 
+}
 
-const login = async (req,res) => { // function User can login
+const login = async (req, res) => { // function User can login
 
     try {
         const user = await UserModel.findOne({
@@ -34,32 +34,49 @@ const login = async (req,res) => { // function User can login
         })
 
         if (!user) return res.status(500).send('Email password incorrect')
-        if(!bcrypt.compareSync(req.body.password, user.password))
-        return res.status(500).send('Email password incorrect')
+        if (!bcrypt.compareSync(req.body.password, user.password))
+            return res.status(500).send('Email password incorrect')
 
         const token = jwt.sign( //Token created
-            {email: user.email}, //Identified user unique
-            
+            { email: user.email }, //Identified user unique
+
             process.env.JWT_SECRET,
-            //{ expiresIn: '7d' } 
 
         )
-        return res.status(200).json({token:token})
-    }catch (error){
+        return res.status(200).json({ token: token })
+    } catch (error) {
         console.log(error)
         res.satatus(500).send('Error login.up')
     }
- }
- async function getUser(req, res) {
+}
+async function getUser(req, res) {
+    try {
+        const user = await UserModel.findByPk(req.params.id)
+        if (user) {
+            return res.status(200).json(user)
+        } else {
+            return res.status(404).send('User not found')
+        }
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+}
+
+async function updateUser(req, res) {
 	try {
-		const user = await UserModel.findByPk(req.params.id)
-		if (user) {
-			return res.status(200).json(user)
+		const [userExist, user] = await UserModel.update(req.body, {
+			returning: true,
+			where: {
+				id: req.params.id,
+			},
+		})
+        if (userExist !== 0) {
+			return res.status(200).json({ message: 'User updated', user: user })
 		} else {
 			return res.status(404).send('User not found')
 		}
 	} catch (error) {
-		res.status(500).send(error.message)
+		return res.status(500).send(error.message)
 	}
 }
 
@@ -69,5 +86,6 @@ const login = async (req,res) => { // function User can login
 module.exports = {
     signup,
     login,
-    getUser
+    getUser,
+    updateUser
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,15 +1,17 @@
-const router = require ('express').Router()
+const router = require('express').Router()
 
 const {
     signup,//comes from auth controller
     login,
-    getUser
-} = require ('../controllers/auth.controller')
+    getUser,
+    updateUser
+} = require('../controllers/auth.controller')
 
 
 
 router.post('/signup', signup)
 router.post('/login', login)
 router.get('/:id', getUser) // get one user
+router.put('/:id', updateUser) 
 
 module.exports = router

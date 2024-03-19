63bb7cc40c0927f29091b34fdc496c4329ef7f51
Diego Fernandez <judifede217@gmail.com>
2024-03-19 10:28:02 +0000

    deleteContribution only admin

--- api/controllers/contribution.controller.js
+++ api/controllers/contribution.controller.js
@@ -65,4 +65,21 @@ const createContribution = async (req, res, amount) => {
     }
 }
 
-module.exports = { stripe, createCheckout, webhook, createContribution }
+async function deleteContribution(req, res) {
+	try {
+		const contribution = await ContributionModel.destroy({
+			where: {
+				id: req.params.id,
+			},
+		})
+		if (contribution) {
+			return res.status(200).json('Contribution deleted')
+		} else {
+			return res.status(404).send('Contribution not found')
+		}
+	} catch (error) {
+		return res.status(500).send(error.message)
+	}
+}
+
+module.exports = { stripe, createCheckout, webhook, createContribution, deleteContribution }
--- api/routes/contribution.route.js
+++ api/routes/contribution.route.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 
-const { checkAuth } = require("../middlewares/checkAuth.js");
+const { checkAuth, checkAdmin } = require("../middlewares/checkAuth.js");
 
 const {
   createCheckout,
   webhook,
+  deleteContribution
 } = require("../controllers/contribution.controller.js");
 
 router.post("/checkout", checkAuth, createCheckout);
 router.post("/webhook", checkAuth, webhook); // Endpoint to recieve Webhook notifications from Stripe
+router.delete("/:id", checkAuth, checkAdmin, deleteContribution);
 
 module.exports = router;

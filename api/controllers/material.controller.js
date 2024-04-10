c1aafae0331f02e944f2169648e2b7f6d871a692
Diego Fernandez <judifede217@gmail.com>
2024-04-10 15:02:01 +0100

    material message

--- api/controllers/material.controller.js
+++ api/controllers/material.controller.js
@@ -32,7 +32,7 @@ async function createMaterial(req, res) {
 		const material = await MaterialModel.create(req.body)
 		return res.status(200).json({ message: 'Material created', material: material })
 	} catch (error) {
-		res.status(500).send(error.message)
+		res.status(500).json({messageError: error.message})
 	}
 }
 
@@ -45,12 +45,12 @@ async function updateMaterial(req, res) {
 			},
 		})
         if (materialExist !== 0) {
-			return res.status(200).send('Material updated')
+			return res.status(200).json({message: 'Material updated'})
 		} else {
-			return res.status(404).send('Material not found or not have changes')
+			return res.status(404).json({messageError: 'Material not found or not have changes'})
 		}
 	} catch (error) {
-		return res.status(500).send(error.message)
+		return res.status(500).json({messageError: error.message})
 	}
 }
 
